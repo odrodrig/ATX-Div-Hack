cd6cfba81bac7710cde4156c448289828d615621
Oliver Rodriguez <ollie@Olivers-MacBook-Pro.local>
2016-10-23 11:55:13 -0500

    adding about page

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,48 @@
-$(document).ready(function(){
+$(function(){
+
+	function getLocations(lat, long) {
+		//Get locations from google
+
+		var map;
+		var infowindow;
+
+		function initMap() {
+		  var location = {lat: lat, lng: long};
+
+		  map = new google.maps.Map(document.getElementById('map'), {
+		    center: location,
+		    zoom: 15
+		  });
+
+		  infowindow = new google.maps.InfoWindow();
+		  var service = new google.maps.places.PlacesService(map);
+		  service.nearbySearch({
+		    location: location,
+		    radius: 500,
+		    type: ['store']
+		  }, callback);
+		}
+
+		function callback(results, status) {
+		  if (status === google.maps.places.PlacesServiceStatus.OK) {
+		    for (var i = 0; i < results.length; i++) {
+		      createMarker(results[i]);
+		    }
+		  }
+		}
+
+		function createMarker(place) {
+		  var placeLoc = place.geometry.location;
+		  var marker = new google.maps.Marker({
+		    map: map,
+		    position: place.geometry.location
+		  });
+
+		  google.maps.event.addListener(marker, 'click', function() {
+		    infowindow.setContent(place.name);
+		    infowindow.open(map, this);
+		  });
+		}
+	}
 
 });
